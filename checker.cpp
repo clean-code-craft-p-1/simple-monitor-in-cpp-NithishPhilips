79be2d71b211ce63da69544802ba91f85da6ba60
NithishPhilips <nithishkumar.punna@philips.com>
2023-08-31 12:41:53 +0530

    fofth

--- checker.cpp
+++ checker.cpp
@@ -1,42 +1,56 @@
 #include <iostream>
 #include <assert.h>
 #include <unistd.h>
+
 using namespace std;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if(temperature > 102 || temperature < 95) {
-    cout << "Temperature critical!\n";
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
-    }
+void flush()
+{
+	for (int i = 0; i < 6; i++)
+	{
+		cout << "\r* " << flush;
+		sleep(1);
+		cout << "\r *" << flush;
+		sleep(1);
+	}
+}
 
-    return 0;
-  } else if(pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
-    }
-    return 0;
-  } else if(spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
-    }
-    return 0;
-  }
-  return 1;
+int TemperatureCheck(float temperature)
+{
+	if (temperature > 102 || temperature < 95) 
+	{
+		cout << "Temperature critical!\n";
+		flush();
+		return 0;
+	}
+	return 1;
+}
+
+int PulseCheck(float pulseRate)
+{
+	if (pulseRate < 60 || pulseRate > 100)
+	{
+		cout << "Pulse Rate is out of range!\n";
+		flush();
+		return 0;
+	}
+	return 1;
+}
+
+int OxygenCheck(float spo2)
+{
+	if (spo2 < 90) 
+	{
+		cout << "Oxygen Saturation out of range!\n";
+		flush();
+		return 0;
+	}
+	return 1;
+}
+
+bool vitalsOk(float temperature, float pulseRate, float spo2) 
+{
+	return (TemperatureCheck(temperature) && PulseCheck(pulseRate) && OxygenCheck(spo2));
 }
 
 int main() {
